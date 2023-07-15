b98d94ff494908f163b810c70664b4901c546c8d
ZKuranbayeva <132888673+ZKuranbayeva@users.noreply.github.com>
2023-07-15 06:41:03 +0500

    )

---
+++ 3-dars/main.py
@@ -0,0 +1,25 @@
+print(f'"Nexia", "Tico", \'Damas\' ko\'rganlar qilar havas')
+
+# 5 ning 4-darajasini toping
+print(5**4)
+
+# 22 ni 4 ga bo'lganda qancha qoldiq qoladi?
+print(22%4)
+
+# Tomonlari 125 ga teng kvadratning yuzi va perimetrini toping
+s = 125**2
+p = 125*4
+print(s)
+print(p)
+
+# Diametri 12 ga teng bo'lgan doiraning yuzini toping
+r = 6
+PI = 3.14
+s = PI * (r**2)
+print(s)
+
+# Katetlari 6 va 7 bo'lgan to'g'ri burchakli uchburchakning gipotenuzasini toping (Pifagor teoremasidan foydalaning)
+a = 6
+b = 7
+c = (6**2 + 7**2)**(1/2)
+print(c)
